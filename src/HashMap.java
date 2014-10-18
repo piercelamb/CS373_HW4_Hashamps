900e0c857b53965653f026924469db40373eab03
piercelamb <richard.pierce.lamb@gmail.com>
2014-10-17 22:53:34 -0700

    Updated some of the methods, still incomplete

--- src/HashMap.java
+++ src/HashMap.java
@@ -8,42 +8,97 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HashMap<K, V> implements Map<K, V> {
-	// TODO: declare my private fields here
+	private static final double MAX_LOAD = 0.75;   // load factor on which to rehash
 	
-	// TODO: comment header
+	private Node[] elements;
+	private int size;
+	
+	
 	public HashMap() {
-		// TODO: implement the constructor
-		
-	}//plamb was here
+		elements = (Node[])new HashMap.Node[10];
+		size = 0;
+	}
 	
 	public void clear() {
-		// TODO: implement this method
+		for (int i = 0; i < elements.length; i++) {
+			elements[i] = null;
+		}
+		size = 0;
 		
 	}
 	
 	public boolean containsKey(K key) {
-		// TODO: implement this method
+		int h = hash(key, elements);
+		Node current = elements[h];
+		while (current != null) {
+			if (current.key.equals(key)) {
+				return true;
+			}
+			//iterate to next linked node
+			current = current.next;
+		}
 		return false;
 	}
 
 	public V get(K key) {
-		// TODO: implement this method
+		int h = hash(key, elements);
+		Node current = elements[h];
+		while (current != null){
+			if (current.key.equals(key)){
+				return current.value;
+			}
+			current = current.next;
+		}
 		return null;
 	}
 
 	public boolean isEmpty() {
-		// TODO: implement this method
-		return false;
+		return size == 0;
 	}
 
 	public Set<K> keySet() {
-		// TODO: implement this method
-		return null;
+		Set<K> keyset = new HashSet<K>();
+		for (int i = 0; i < elements.length; i++) {
+			Node current = elements[i];
+			//this is O(N^2) tho the spec says O(N) wot
+			while (current != null) {
+				keyset.add(current.key);
+				current = current.next;
+			}
+		}
+		return keyset;
 	}
 	
 	public void put(K key, V value) {
-		// TODO: implement this method
+		int h = hash(key, elements);
+		Node current = elements[h];
+		//index is null so add new Node
+		if (current == null){
+			current.key = key;
+			current.value = value;
+			current.next = null;
+		}
+		//check to see if existing nodes have the same key to replace
+		boolean replaced = false;
+		while (current != null){
+		if (current.key.equals(key)) {
+			current.key = key;
+			current.value = value;
+			replaced = true;
+		}
+		current = current.next;
+		}
+		//add to front of existing linked list
+		//p sure this is wrong
+		if (replaced == false) {
+			Node newNode = new Node(key, value); 
+			newNode = elements[h];
+		}
 		
+		// resize if necessary
+		if (loadFactor() > MAX_LOAD) {
+					rehash();
+		}
 	}
 	
 	public void remove(K key) {
@@ -52,15 +107,48 @@ public class HashMap<K, V> implements Map<K, V> {
 	}
 
 	public int size() {
-		// TODO: implement this method
-		return 0;
+		return size;
 	}
+	
 	
 	public String toString() {
 		// TODO: implement this method
 		return "TODO";
 	}
 	
-	// TODO: declare any private helpers, inner classes, etc. here
+	private void rehash() {
+		Node[] newElements = (Node[]) new HashMap.Node[2 * this.elements.length];
+		Node[] old = this.elements;
+		size = 0;
+		for (Node node : old) {
+			while (node != null) {
+				int h = hash(node.key, newElements);
+				newElements[h] = node;
+				//might be wrong, might require your put() method to add nodes appropriately
+				node = node.next;
+			}
+			size++;
+		}
+		this.elements = newElements;
+	}
+	
+	private double loadFactor() {
+		return (double) size / elements.length;
+	}
+	
+	private int hash(K key, Node[] elements) {
+		return Math.abs(key.hashCode()) % elements.length;
+	}
+	
+	private class Node {
+		public K key;
+		public V value;
+		public Node next;
+		
+		public Node(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
 	
 }
